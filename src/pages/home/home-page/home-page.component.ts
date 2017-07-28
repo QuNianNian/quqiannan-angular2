0ffe77fdfe016d8154f6046e381e23185106564d
Qu <qiannan.qu@accenture.com>
2017-07-28 08:58:06 +0800

    homepage table

--- src/pages/home/home-page/home-page.component.ts
+++ src/pages/home/home-page/home-page.component.ts
@@ -9,6 +9,7 @@ import { Meta, Title } from '@angular/platform-browser';
 export class HomePageComponent implements OnInit {
   users:Array<any> = [
     {"index":19,
+    "romIndex": 19,
     "name":"刘雨飏",
       "sex":"Male",
       "name_en":"Ryane",
@@ -17,6 +18,7 @@ export class HomePageComponent implements OnInit {
       "exam2":66,
       "exam3":66},
       {"index":7,
+      "romIndex": 7,
       "name":"小明",
       "sex":"Male",
       "name_en":"Ryane",
@@ -25,6 +27,7 @@ export class HomePageComponent implements OnInit {
       "exam2":66,
       "exam3":66},
       {"index":14,
+      "romIndex": 14,
       "name":"Chunk",
       "sex":"Male",
       "name_en":"Chunk",
@@ -33,12 +36,16 @@ export class HomePageComponent implements OnInit {
       "exam2":76,
       "exam3":76}
   ]
+
+
+
 deleteLast(){
   this.users.pop()
 }
 saveNewUser(){
   this.users.push({
      "index":666,
+     "romIndex":666,
       "name":"New User",
       "sex":"Male",
       "name_en":"new_user",
@@ -48,14 +55,32 @@ saveNewUser(){
       "exam3":76
   })
 }
+
   sortByAsccending(){
     // 参考MDN Array操作的API文档 Array相关方法方法
+    this.users.sort(function (a, b){
+      return a.index - b.index;
+    });
+
   }
+
   sortByDesccending(){
+    this.users.sort(function (a, b){
+      return b.index - a.index;
+    });
+    
     // 参考MDN Array操作的API文档 Array相关方法
     // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
   }
   sortByRadom(){
+    this.users.forEach(user=>{ 
+      user.romIndex = Math.random();
+    });
+
+    this.users.sort(function (a, b){
+      return a.romIndex - b.romIndex;
+    });
+    
     // 参考MDN Array操作的API文档 Math相关方法
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
   }
